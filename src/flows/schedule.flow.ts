b53e7ff023a9d2d68ad13762e99bfd4e591bb912
Octavio <jorgeot19@gmail.com>
2024-11-24 13:05:06 +0100

    Update 68

--- src/flows/schedule.flow.ts
+++ src/flows/schedule.flow.ts
@@ -56,10 +56,33 @@ const flowSchedule = addKeyword(EVENTS.ACTION).addAction(async (_, { extensions,
     const isDateAvailable = listParse.every(({ fromDate, toDate }) => !isWithinInterval(desiredDate, { start: fromDate, end: toDate }));
 
     if (!isDateAvailable) {
-        const m = 'Lo siento, esa hora ya está reservada. ¿Alguna otra fecha y hora?';
+        const MINUTES_INCREMENT = 15;
+        const dateTwo = addMinutes(desiredDate, MINUTES_INCREMENT);
+        const isDateAvailable2 = listParse.every(({ fromDate, toDate }) => !isWithinInterval(dateTwo, { start: fromDate, end: toDate })
+        );
+        
+        if (!isDateAvailable2) {
+            console.log('Fecha no disponible, revisando incrementos...');
+            const m = 'Lo siento, esa hora ya está reservada. ¿Alguna otra fecha y hora?';
+
+            await handleHistory({ content: m, role: 'assistant' }, state);
+            await state.update({ desiredDate });
+        
+            const chunks = m.split(/(?<!\d)\.\s+/g);
+            for (const chunk of chunks) {
+                await flowDynamic([{ body: chunk.trim(), delay: generateTimer(150, 250) }]);
+            }
         await flowDynamic(m);
         await handleHistory({ content: m, role: 'assistant' }, state);
         return endFlow()
+        } else{
+            const formattedDateFrom = format(dateTwo, 'hh:mm a');
+            const formattedDateTo = format(addMinutes(dateTwo, +DURATION_MEET), 'hh:mm a');
+            const m2 = `Lo siento, la hora seleccionada no está disponible. ¿Te parece bien agendar de ${formattedDateFrom} a ${formattedDateTo} el día ${format(desiredDate, 'dd/MM/yyyy')}? *si*`;    
+            await flowDynamic(m2);
+            await handleHistory({ content: m2, role: 'assistant' }, state);
+            return endFlow();
+        }
     }
 
     const formattedDateFrom = format(desiredDate, 'hh:mm a');
