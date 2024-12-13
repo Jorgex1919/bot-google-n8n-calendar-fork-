191bf5d68de2ef7bb4457653dd5c4961d3144e37
Octavio <jorgeot19@gmail.com>
2024-12-13 20:26:16 +0100

    Update 88

--- src/flows/schedule.flow.ts
+++ src/flows/schedule.flow.ts
@@ -80,7 +80,7 @@ const flowSchedule = addKeyword(EVENTS.ACTION).addAction(async (_, { gotoFlow, e
             await state.update({ desiredDate })
 
 
-            const Confirm2 = addKeyword().addAction(async (_, { flowDynamic, state }) => {
+            const Confirm2 = addKeyword(EVENTS.ACTION).addAction(async (_, { flowDynamic, state }) => {
                 console.log('Estamos en flowConfirmDos');
                 await flowDynamic(m2)
             }).addAction({ capture: true }, async ({ body }, { gotoFlow, flowDynamic, state }) => {
