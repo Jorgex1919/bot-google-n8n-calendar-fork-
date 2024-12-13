4788e5f15f3c4a5deae6da7171d0470a36cdfdfe
Octavio <jorgeot19@gmail.com>
2024-12-13 15:07:30 +0100

    Update 83

--- src/flows/confirmdos.flow.ts
+++ src/flows/confirmdos.flow.ts
@@ -3,8 +3,6 @@ import { clearHistory } from "../utils/handleHistory";
 import { addMinutes, format } from "date-fns";
 import { utcToZonedTime } from "date-fns-tz";
 import { appToCalendar } from "src/services/calendar";
-import { getHistoryParse, handleHistory } from "../utils/handleHistory";
-import { flowSchedule } from "./schedule.flow";
 
 const DURATION_MEET = process.env.DURATION_MEET ?? 45
 const TIME_ZONE = process.env.TZ
@@ -14,27 +12,6 @@ const TIME_ZONE = process.env.TZ
 console.log('Entrando a flowConfirmDos...');
 
 const flowConfirmDos = addKeyword(EVENTS.ACTION).addAction(async (_, { flowDynamic, state }) => {
-    const MINUTES_INCREMENT = 15;
-        const dateTwo = addMinutes(state.get('desiredDate'), MINUTES_INCREMENT);
-    const formattedDateFrom = format(dateTwo, 'hh:mm a');
-            const formattedDateTo = format(addMinutes(dateTwo, +DURATION_MEET), 'hh:mm a');
-            const m2 = `Lo siento, la hora seleccionada no está disponible. ¿Te parece bien agendar de ${formattedDateFrom} a ${formattedDateTo} el día ${format(desiredDate, 'dd/MM/yyyy')}? *si*`;    
-            await flowDynamic(m2);
-            await handleHistory({ content: m2, role: 'assistant' }, state);
-            
-            flowSchedule.addAction({ capture: true }, async ({ body }, { gotoFlow, flowDynamic, state }) => {
-
-                console.log('Redirigiendo a flowConfirmDos...');
-                if (body.toLowerCase().includes('si')) {
-                    await flowDynamic('Ok, voy a pedirte unos datos para agendar')
-                    await flowDynamic('¿Cual es tu nombre?')
-                }
-                if (body.toLowerCase().includes('sí')) return gotoFlow(flowConfirmDos)
-                if (body.toLowerCase().includes('ok')) return gotoFlow(flowConfirmDos)
-            
-                await flowDynamic('¿Alguna otra fecha y hora?')
-                await state.update({ desiredDate: null })
-            })
     await flowDynamic('Ok, voy a pedirte unos datos para agendar')
     await flowDynamic('¿Cual es tu nombre?')
 }).addAction({ capture: true }, async (ctx, { state, flowDynamic, endFlow }) => {
