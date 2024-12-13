73febbf15c635a03517638ff55f6f9ad26fe2cf0
Octavio <jorgeot19@gmail.com>
2024-12-13 14:14:22 +0100

    Update 81

--- src/flows/schedule.flow.ts
+++ src/flows/schedule.flow.ts
@@ -77,7 +77,9 @@ const flowSchedule = addKeyword(EVENTS.ACTION).addAction(async (_, { extensions,
             const m2 = `Lo siento, la hora seleccionada no está disponible. ¿Te parece bien agendar de ${formattedDateFrom} a ${formattedDateTo} el día ${format(desiredDate, 'dd/MM/yyyy')}? *si*`;    
             await flowDynamic(m2);
             await handleHistory({ content: m2, role: 'assistant' }, state);
-            await state.update({ desiredDate }).addAction({ capture: true }, async ({ body }, { gotoFlow, flowDynamic, state }) => {
+            await state.update({ desiredDate })
+            
+            flowSchedule.addAction({ capture: true }, async ({ body }, { gotoFlow, flowDynamic, state }) => {
 
                 console.log('Redirigiendo a flowConfirmDos...');
                 if (body.toLowerCase().includes('si')) return gotoFlow(flowConfirmDos)
